b65f142f6ba3df4c4d070dde1a4e1e62d4a29913
Alexander Vladyko <vladyko.alexander@gmail.com>
2022-01-23 06:19:26 +0300

    L17 ask to continue game afterwords

--- unknown-number-game-console/main.swift
+++ unknown-number-game-console/main.swift
@@ -5,21 +5,32 @@
 //  Created by Александр Владыко on 19.01.22.
 //
 
-let randomNumber = UInt8.random(in: 1...250)
-print("Machine have chosen the number. Try to guess it!")
+var userWantToPlay: Bool = true
 
-while true {
-    guard let unwrappedValue = UInt8(readLine() ?? "") else {
-        print("Number isn't correct, please, try another one")
-        continue
+while userWantToPlay {
+    
+    let randomNumber = UInt8.random(in: 1...250)
+    print("Machine have chosen the number. Try to guess it!")
+    
+    while true {
+        guard let unwrappedValue = UInt8(readLine() ?? "") else {
+            print("Number isn't correct, please, try another one")
+            continue
+        }
+        
+        if unwrappedValue > randomNumber {
+            print("Smaller!")
+        } else if unwrappedValue < randomNumber {
+            print("Bigger!")
+        } else {
+            print("Correct!")
+            userWantToPlay = false
+            break
+        }
     }
-
-    if unwrappedValue > randomNumber {
-        print("Smaller!")
-    } else if unwrappedValue < randomNumber {
-         print("Bigger!")
-    } else {
-        print("Correct!")
-        break
+    
+    print("Wanna play a game again? Type 'y' if you do!")
+    if let userAnswer = readLine() {
+        userWantToPlay = userAnswer == "y" ? true : false
     }
 }
